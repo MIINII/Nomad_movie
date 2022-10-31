9ebbb762ac5ff103322e2a3f77f127195fab5f5a
MIINII <ft.dawn.o@gmail.com>
2022-10-31 22:21:56 +0900

    :sparkles: Feat : Todo 컴포넌트 만들어보기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import GalConverter from './components/GalConverter';
+import GalConverter from './prac/GalConverter';
 import TimeConverter from './components/TimeConverter';
+import Todo from './prac/Todo';
 
 function App() {
-  const [nowConverter, setNowConverter] = useState('');
-  const [index, setIndex] = useState('x');
-  const onSelect = e => {
-    setIndex(e.target.value);
-  };
-
   return (
     <div className='App'>
-      <h1>컨버트ㅓ</h1>
-      <select value={index} onChange={onSelect}>
-        <option value='x'>어떤걸 변환하실건지..?</option>
-        <option value='0'>시간 </option>
-        <option value='1'>Gal</option>
-      </select>
-      <hr />
-      {index === 'x' ? '선택하쇼' : null}
-      {index === '0' ? <TimeConverter /> : null}
-      {index === '1' ? <GalConverter /> : null}
+      <Todo />
     </div>
   );
 }
---
+++ src/prac/Todo.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const Todo = () => {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const onChange = e => {
+    setTodo(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (todo === '') {
+      return;
+    }
+    setTodos(currentArr => [todo, ...currentArr]);
+    setTodo('');
+  };
+
+  return (
+    <>
+      <h1>내할일({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={todo} type='text' placeholder='쓰쇼' />
+        <button>추가하쇼</button>
+      </form>
+    </>
+  );
+};
+
+export default Todo;
