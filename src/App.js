e69e1cf0b88ee04eb6f2ff39c6e26d55f90d5b9b
MIINII <ft.dawn.o@gmail.com>
2022-11-08 01:34:55 +0900

    :sparkles: Feat : Route 설정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-// import './App.css';
-import GalConverter from './prac/GalConverter';
-import Todo from './prac/Todo';
-import CoinTracker from './prac/CoinTracker';
-import Movie from './prac/Movie';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Movie from './components/Movie';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 
 function App() {
   return (
-    <div className='App'>
-      {/* <Todo /> */}
-      {/* <CoinTracker /> */}
-      <Movie />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movie' element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
