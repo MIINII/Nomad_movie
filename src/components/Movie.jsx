9d21c4727433859b836521f0d4828cec95f46efc
MIINII <ft.dawn.o@gmail.com>
2022-11-09 12:32:27 +0900

    :alembic: use : useParams() 사용해보기

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,11 +1,14 @@
 // 어떤 props를 가지고 있는지 알기위해서
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Movie = ({ coverImg, title, summary, genres }) => {
+const Movie = ({ id, coverImg, title, summary, genres }) => {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map(genre => (
@@ -17,6 +20,7 @@ const Movie = ({ coverImg, title, summary, genres }) => {
 };
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,4 +1,10 @@
+import { useParams } from 'react-router-dom';
+
 const Detail = () => {
+  // url에 있는 값을 반환해주는 기본 함수(특히 변경되는 값)
+  const { id } = useParams();
+  console.log('🚀 ⁝ Detail ⁝ id', id);
+
   return (
     <>
       <h1>디테일</h1>
